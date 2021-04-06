883cf86e33ab447f1ba0fc6f03264c57770ab59d
Micah Cooper <micah+github@mrmicahcooper.com>
2021-04-05 23:31:31 -0400

    Setup service, ingress

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :delixir, DelixirWeb.Endpoint,
-  url: [host: "localhost", port: 80],
+  url: [host: "delixir.clank.us", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
