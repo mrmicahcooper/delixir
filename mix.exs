9c84ddef0c5a2509ab1b0076fe85432105a9d975
Micah Cooper <micah+github@mrmicahcooper.com>
2021-04-06 23:13:32 -0400

    view nodes

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule Delixir.MixProject do
       {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
+      {:libcluster, "3.2.2"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
